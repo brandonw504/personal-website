f9644c9b9ba1e412aa3f78ad1f954e02f40b4c75
brandonw504 <71607013+brandonw504@users.noreply.github.com>
2022-09-13 16:33:05 -0700

    removed animated cursor

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,12 @@ import About from './about'
 import Experience from './experience'
 import Projects from './projects'
 import Footer from './footer';
-import AnimatedCursor from "react-animated-cursor";
 
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
         <div className={styles.wrapper}>
-            <AnimatedCursor color="255, 255, 255" outerScale={4} />
             <Head>
                 <title>Brandon Wong</title>
             </Head>
