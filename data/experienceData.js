04b77365fd565eace2eef4603bb54ad7ab3452f2
brandonw504 <71607013+brandonw504@users.noreply.github.com>
2022-12-16 17:28:07 -0800

    updated resume

--- data/experienceData.js
+++ data/experienceData.js
@@ -4,8 +4,9 @@ const experienceData = [
         title: "Software Engineer @ ",
         time: "Sept 2021 - Jun 2022",
         description: [
-            "Built and styled a dynamic art gallery website using Next.js.",
-            "Learned industry web development practices in React.js.",
+            "Built and styled a dynamic and responsive art gallery website using Next.js.",
+            "Used server-side rendering for SEO and improved page loading.",
+            "Collaborated with back-end developers on the art piece API.",
             "Managed several engineers' changes to the codebase using Github."
         ]
     }
